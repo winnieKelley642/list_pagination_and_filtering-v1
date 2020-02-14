bb766222c37cfa80b45a89ca8495de9a72afd08c
Winnie Kelley <alldayallnight642@icloud.com>
2020-02-14 07:53:20 -0500

    This commit has psuedo code and initail showPage function
    This commit will show only the first 10 students and hide the rest.

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # list_pagination_and_filtering-v1
- This is project 2 of Treehouse FSJS Techdegree
+ This is project 2 of Treehouse FSJS Techdegree - List Pagination and Filtering
+
+ I am going for the grade of exceeding expectations
+
+ Treehouse provided me with these starter project files:
+ - HTML files:
+    - index.html
+    - Example files:
+        - 44students.html
+        - 66students.html
+        - example-exceeds.html
+        - example-meets.html
+ - CSS files:
+    - design.css
+    - reset.css
+ - JS file:
+    - script.js (This is the one I'm editing)
+
+To ex0ceed expectations Treehouse would like the following:
+- NO SNIPPETS, PLUGINS OR LIBRARIES:
+    - contains only plain "vanilla" JavaScript and doesn't rely on jQuery, some other library, or any code snippets or plugins.
+
+- PROGRESSIVE ENCHANCEMENT & UNOBSTRUSIVE JAVASCRIPT
+    - No inline JavaScript. All JavaScript is linked from an external file.
+    - Use unobstrusive JavaScript to append markup for the pagination links. So none of the pagination link's markup is added into HTML.
+    - Exceeds expectations by:
+        - Use unobstrusive JavaScript to append HTML for a search bar
+
+- PAGINATION LINKS:
+    - Pagination Links are created. If there are 44 students, 5 links should be generated, if there's 66 students, 7 links should be generated. Etc.
+    - Exceeds expectations by:
+        - Pagination links display based on how many search results are returned. For example: if 10 or fewer results are returned, 0 or 1 pagination links are displayed. If 22 search results are returned, 3 pagination links are displayed.
+
+- PAGING:
+    - This first 10 students are shown when the page loads, and each pagination link displays the correct students.
+    - Clicking on "1" in the pagination links should show students 1 to 10. Clicking "2" shows 11 to 20. Clicking "5" shows students 41 to 50, and so on.
+    - Exceeds expectations by:
+        - When a search yields 0 results, a message is displayed on the page, informing the user that no result have been found.
+
+- CODE COMMENTS:
+    - Code comments have been added explaining how the function works
+
+Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
--- js/script.js
+++ js/script.js
@@ -1,10 +1,26 @@
 /******************************************
 Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
+
+Psuedo code written based on Treehouse Study guide and Instructions
 ******************************************/
    
 // Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
+/**
+ * I will be starting this project with a list of 54 student 
+ * names, photos and email address.
+ * 
+ * Create something that will only show 10 students at a time.
+ * 
+ * Create a navigation system, a series of links at the bottom 
+ * of the page.
+ * 
+ * Make sure that this will work with JavaScript turned off.
+ * 
+ * Make sure that this project will work with any number of 
+ * students.
+ */
 
 /*** 
    Add your global variables that store the DOM elements you will 
@@ -16,9 +32,16 @@ FSJS project 2 - List Filter and Pagination
    will only be used inside of a function, then it can be locally 
    scoped to that function.
 ***/
+//Global variables:
 
+// variable to get and store the list of students:
+const listOfStudents = document.querySelectorAll('.student-item');
 
+//testing purpose only
+console.log(`list of students = ${listOfStudents}`);
 
+//variable to store the max number of students that can be shown per page (10)
+const numberOfStudentsPerPage = 10;
 
 /*** 
    Create the `showPage` function to hide all of the items in the 
@@ -35,16 +58,128 @@ FSJS project 2 - List Filter and Pagination
        "invoke" the function 
 ***/
 
+/**
+ * DISPLAYING A PAGE
+ * (Using console to test the function in the earlier stages)
+ * 1. Create a function that:
+ *    1a. function hidides all the students other than the 10 
+ *        students that are being displayed.
+ *    1b. function should have two parameter:
+ *       - list : the list of students that's being passed in 
+ *                as an argument when calling the function.
+ *       - page : the page number that's being passed in as an 
+ *                argument when calling the function.
+ * 
+ * 2. Create two variables (This is so it works with any 
+ *    length list).
+ *    2a. Start Index = (page parameter * items per page) - 
+ *       items per page.
+ *    2b. End Index = page parameter * items per page
+ * 
+ * 3. Create a loop that loops over the list
+ *    3a. display items that are:
+ *       - greater than or equal to start index
+ *       AND
+ *       - less than the end index.
+ *    3b. hide items that doesn't fit above parameters
+ */
 
+//DISPLAYING A PAGE:
+//Create a function showPage, that has two parameters (list, page) 
+const showPage = (listOfStudents, page) => {
+   //create 2 variables startIndex and endIndex so that this will work for any length list of students (variables and list provided by Treehouse)
+   const startIndex = (page * numberOfStudentsPerPage) - numberOfStudentsPerPage;
+   const endIndex = (page * numberOfStudentsPerPage);
 
+   console.log(`Succeffully called the function`);
+   console.log(`Before going into the loop:`);
+   console.log(`# of students in listOfStudents: ${listOfStudents.length}`)
+   console.log(`page: ${page}`);
+   console.log(`numberOfStudentsPerPage: ${numberOfStudentsPerPage}`);
+   console.log(`startIndex: ${startIndex}`);
+
+   //create a loop that will loop over the list
+   for (let i = 0; i < listOfStudents.length; i++){
+      console.log(`in the first loop of the showPage function`);
+     // consolee.log (`i is: ${i}`);
+      //create an if statement to check to see where we are in the loop so we can display 10 students and hide the rest
+      if ( i >= startIndex && i < endIndex){
+         console.log(`the loop is within the start and end index.`);
+         //display these students
+         listOfStudents[i].style.display = '';
+         console.log(`i is now ${i}`);
+      } else{
+         console.log(`This is outside the number of students display on each page`);
+         //hide the students beyound the 10 that's being displayed
+         listOfStudents[i].style.display = 'none';
+      }
+   }
+}
 
 /*** 
    Create the `appendPageLinks function` to generate, append, and add 
    functionality to the pagination buttons.
 ***/
 
+/**
+ * ADDING PAGINATION LINKS
+ * 1. Create a function that:
+ *    1a. function that creates and appends functioning
+ *        pagination links.
+ *    1b. accepts a single "list" parameter to represent the 
+ *        actual list of students.
+ *    1c. Inside the function:
+ *       - Create and append DOM elements for pagination links;
+ *       - use lines 119 - 137 in examples/example-meets.thml 
+ *         as template.
+ *       - watch out for:
+ *          - how elements are nested.
+ *          - necessary class names.
+ *          - other elements attributes.
+ *          - where additions should be append.
+ * 
+ * 2. Add active class to the first pagination link initially.
+ * 3. Add a "click" event listneer to each A element. A loop 
+ *    can be helpful here.
+ *    3a. When the pagination link is clicked:
+ *       - active class name should be removed from all 
+ *         pagination links (use a loop).
+ *       - active calss name should be added to the link that 
+ *         was just clicked. (use target property of the 
+ *         event object).
+ *       - showPage function should be called passing in 
+ *         arguments
+ * 
+ * RESULTS:
+ * - container div element with class name "pagination"
+ *    - append to div element with class name of page.
+ * - nested UL element containing one LI element for every 10 
+ *    students in the list.
+ * 
+ * TIP:
+ * Divide the list.length by the max number of items per page 
+ * to figure out how many pages are needed, and can use a loop 
+ * that iterates that many times to create the correct number 
+ * of LI elements.
+ * 
+ * - Each LI element should contain an A element with an href 
+ * attribute of # and text set to the page number each link 
+ * will show. First link is 1. Second link is 2. And so on.
+ * 
+ * - Loop index can be helpful in setting the text of the 
+ * pagination links.
+ * 
+ */
 
+ /**
+  * Program needs to work with any number of list. test using 44.students.html and 64students.html
+  */
 
-
+  //Temporarily call showPage functino here just for testing purposes:
+  showPage(listOfStudents, 1);
+ /**
+  * Add Exceed expectation here after I complete meet 
+  * expectations
+  */
 
 // Remember to delete the comments that came with this file, and replace them with your own code comments.
