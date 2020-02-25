61ca10a61cc8e4533b4ba5f8189dbaaf594af583
Winnie Kelley <alldayallnight642@icloud.com>
2020-02-25 06:10:16 -0500

    not working in many ways
    search is still not working
     - almost works by luck
     - one try it'll show next it won't
     - backspace confuses the search
     - have to reload for every search

--- js/script.js
+++ js/script.js
@@ -49,6 +49,9 @@ let currentPageNumber = 1;
 //variable to store information for searching students
 let searchList = [];
 
+//empty list for removing all pages for no results found
+let emptyList = [];
+
 //create a search bar using lines 16 - 19 from examples/example-exceeds.html as a template
 //create a variable to get and store 'page-header'
 var pageHeaderDiv = document.querySelector('.page-header');
@@ -78,7 +81,9 @@ searchButton.addEventListener('click', (e)=>{
 
 //add input eventListener for up key:
 userSearchInput.addEventListener('keyup', (e) => {
-	console.log(`in keyup event listener`);
+   console.log(`in keyup event listener`);
+   search(searchList);
+   console.log(`keyup search complete`);
 });
 
 //function for search button
@@ -91,6 +96,8 @@ const search = (searchList) =>{
    //create a loop to compare user input to each student
    for(let i = 0; i < listOfStudents.length; i++){
       console.log(`in i loop for search`);
+      listOfStudents[i].style.display = 'none';
+
       //create a variable to store a name one name at a time
       var name = allNames[i];
       console.log(`name is: ${name.textContent}`);
@@ -102,173 +109,20 @@ const search = (searchList) =>{
          console.log(`comparing userInput to list`);
          console.log(`there's a match`);
          console.log(`current student: ${listOfStudents[i].textContent}`);
+         //hide all students
+         showPage(emptyList, 0);
+         //hide all page links
+         appendPageLinks(emptyList);
          searchList.push(listOfStudents[i]);
          console.log(`search list is now: ${searchList.length}`);
       } else{
          console.log(`does not match`);
       }
+      showPage(searchList, 1);
    }
-   showPage(searchList, 1);
+   appendPageLinks(searchList);
 }
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
 
-/**
- * ADDING PAGINATION LINKS
- * 1. Create a function that:
- *    1a. function that creates and appends functioning
- *        pagination links.
- *    1b. accepts a single "list" parameter to represent the 
- *        actual list of students.
- *    1c. Inside the function:
- *       - Create and append DOM elements for pagination links;
- *       - use lines 119 - 137 in examples/example-meets.thml 
- *         as template.
- *       - watch out for:
- *          - how elements are nested.
- *          - necessary class names.
- *          - other elements attributes.
- *          - where additions should be append.
- * 
- * 2. Add active class to the first pagination link initially.
- * 3. Add a "click" event listneer to each A element. A loop 
- *    can be helpful here.
- *    3a. When the pagination link is clicked:
- *       - active class name should be removed from all 
- *         pagination links (use a loop).
- *       - active calss name should be added to the link that 
- *         was just clicked. (use target property of the 
- *         event object).
- *       - showPage function should be called passing in 
- *         arguments
- * 
- * RESULTS:
- * - container div element with class name "pagination"
- *    - append to div element with class name of page.
- * - nested UL element containing one LI element for every 10 
- *    students in the list.
- * 
- * TIP:
- * Divide the list.length by the max number of items per page 
- * to figure out how many pages are needed, and can use a loop 
- * that iterates that many times to create the correct number 
- * of LI elements.
- * 
- * - Each LI element should contain an A element with an href 
- * attribute of # and text set to the page number each link 
- * will show. First link is 1. Second link is 2. And so on.
- * 
- * - Loop index can be helpful in setting the text of the 
- * pagination links.
- * 
- */
-
- const appendPageLinks = (listOfStudents) => {
-    //Testing
-   //  console.log(`appendPageLinksFunction working`);
-   //  console.log(`in the appendPageLinks function`);
-   //  console.log(`length of list of students: ${listOfStudents.length} students`);
-   //  console.log(`number of students per page allowed: ${numberOfStudentsPerPage} students`);
-
-    //create a variable to store the total number of pages needed
-    const numberOfPages = Math.ceil(listOfStudents.length / numberOfStudentsPerPage);
-    //testing:
-   //  console.log(`number of pages needed for this list: ${numberOfPages} pages`);
-    
-    //Using lines 119 - 137 in examples/example-meets.html as a template:
-    //create a variable to get and store the '.page' div
-    var pageDiv = document.querySelector('.page');
-    //create a variable and create a div in it and call it "pagination"
-    var paginationDiv = document.createElement('div');
-    paginationDiv.setAttribute('class', 'pagination');
-    pageDiv.appendChild(paginationDiv);
-
-    //create variable to create a 'ul' in paginationDiv and give it a class of 'pagination-ul'
-    var ulDiv = document.createElement('ul');
-    ulDiv.setAttribute('class', 'pagination-ul');
-    paginationDiv.appendChild(ulDiv);
-
-    //create a loop to create 'li' and 'a' for each student
-    //testing
-   //  console.log(`number of pages: ${numberOfPages}`);
-    for(let i  = 1; i <= numberOfPages; i++)
-    {
-      // console.log(`in the loop for creating 'li' and 'a' for each student`);
-
-      //create a variable to create and store 'li' in 'ulDiv' and give it class 'pagiation-li'
-      var liPagination = document.createElement('li');
-      liPagination.setAttribute('class', 'pagination-li');
-      ulDiv.appendChild(liPagination);
-
-      //create a variable to create and store 'a' in 'li' and give it class 'pagination-a'
-      var aPagination = document.createElement('a');
-      aPagination.setAttribute('class', 'pagination-a');
-      //add page number
-      aPagination.textContent = (i);
-      liPagination.appendChild(aPagination);
-
-      //when the page initially loads, it should always start on page 1, so the active page should always start on page 1
-      //testing
-      // console.log(`current page number = ${currentPageNumber}`);
-
-      //make sure that active page is 1 and set the colour to it for being the active page
-      if(currentPageNumber === 1){
-         aPagination.classList.add('active');
-         //change currentPageNumber value so only page 1 is active
-         currentPageNumber++;
-      }
-    }
-
-    //So this is to make sure that the active is cleared out of all buttons and assigned to the one that's pressed, but I was having a lot of trouble. After getting help on Slack, @Juan L, helped by giving me a little bit of a psuedo code for this part to fix my issues
-
-    //Gather all the 'a' tags and store in a variable (suggested by @Juan L)
-    const aLinks = document.querySelectorAll('a');
-    //testing
-   //  console.log(aLinks);
-
-    //create a loop(i) to add eventListeners for those a tags (suggested by @Juan L)
-    for(let i = 0; i < aLinks.length; i++){
-      //  console.log(`in the i loop`)
-       // When a tag is clicked (suggested by @Juan L)
-       aLinks[i].addEventListener('click', (e) =>{
-          //Add an inner loop (j) to loop through all the 'a' tags to remove the classes (suggested by @Juan L)
-
-          //had to take out the for loop (j) suggestion, because after trying out a bunch of different variations of the for loop (j) it didn't work, but works without it.
-          //for(let j = 0; j < aLinks.length; j++){
-             //console.log(`in the j loop`);
-             
-             //create a variable to store the the 'a'
-             const activePage = document.querySelector('.active');
-             //testing
-            //  console.log(activePage);
-             
-             //remove the 'active' by changing it to ''(suggested by @Juan L)
-             activePage.className = ('');
-             //testing
-             //console.log(`leaving the j loop`);
-          //}
-
-          //Back in the outer loop add back the class of active to whatever link was clicked and call showPage (suggseted by @Juan L)
-
-          //create a variable to get and store which button was clicked
-          const buttonClicked = e.target;
-
-          //update current page number to pass into the showPage function
-          currentPageNumber = buttonClicked.textContent;
-          //testing
-         //  console.log(`page number is: ${currentPageNumber}`);
-
-          //add 'active' class onto the button clicked
-          buttonClicked.className = 'active';
-         
-          //display the students based on the page number in currentPageNumber
-          showPage(listOfStudents, currentPageNumber);
-       });
-    }
-
- }
 /*** 
    Create the `showPage` function to hide all of the items in the 
    list except for the ten you want to show.
@@ -341,6 +195,166 @@ const showPage = (listOfStudents, page) => {
          listOfStudents[i].style.display = 'none';
       }
    }
+}
+
+/*** 
+   Create the `appendPageLinks function` to generate, append, and add 
+   functionality to the pagination buttons.
+***/
+
+/**
+ * ADDING PAGINATION LINKS
+ * 1. Create a function that:
+ *    1a. function that creates and appends functioning
+ *        pagination links.
+ *    1b. accepts a single "list" parameter to represent the 
+ *        actual list of students.
+ *    1c. Inside the function:
+ *       - Create and append DOM elements for pagination links;
+ *       - use lines 119 - 137 in examples/example-meets.thml 
+ *         as template.
+ *       - watch out for:
+ *          - how elements are nested.
+ *          - necessary class names.
+ *          - other elements attributes.
+ *          - where additions should be append.
+ * 
+ * 2. Add active class to the first pagination link initially.
+ * 3. Add a "click" event listneer to each A element. A loop 
+ *    can be helpful here.
+ *    3a. When the pagination link is clicked:
+ *       - active class name should be removed from all 
+ *         pagination links (use a loop).
+ *       - active calss name should be added to the link that 
+ *         was just clicked. (use target property of the 
+ *         event object).
+ *       - showPage function should be called passing in 
+ *         arguments
+ * 
+ * RESULTS:
+ * - container div element with class name "pagination"
+ *    - append to div element with class name of page.
+ * - nested UL element containing one LI element for every 10 
+ *    students in the list.
+ * 
+ * TIP:
+ * Divide the list.length by the max number of items per page 
+ * to figure out how many pages are needed, and can use a loop 
+ * that iterates that many times to create the correct number 
+ * of LI elements.
+ * 
+ * - Each LI element should contain an A element with an href 
+ * attribute of # and text set to the page number each link 
+ * will show. First link is 1. Second link is 2. And so on.
+ * 
+ * - Loop index can be helpful in setting the text of the 
+ * pagination links.
+ * 
+ */
+
+const appendPageLinks = (listOfStudents) => {
+   //Testing
+  //  console.log(`appendPageLinksFunction working`);
+  //  console.log(`in the appendPageLinks function`);
+  //  console.log(`length of list of students: ${listOfStudents.length} students`);
+  //  console.log(`number of students per page allowed: ${numberOfStudentsPerPage} students`);
+
+   //create a variable to store the total number of pages needed
+   const numberOfPages = Math.ceil(listOfStudents.length / numberOfStudentsPerPage);
+   //testing:
+  //  console.log(`number of pages needed for this list: ${numberOfPages} pages`);
+   
+   //Using lines 119 - 137 in examples/example-meets.html as a template:
+   //create a variable to get and store the '.page' div
+   var pageDiv = document.querySelector('.page');
+   //create a variable and create a div in it and call it "pagination"
+   var paginationDiv = document.createElement('div');
+   paginationDiv.setAttribute('class', 'pagination');
+   pageDiv.appendChild(paginationDiv);
+
+   //create variable to create a 'ul' in paginationDiv and give it a class of 'pagination-ul'
+   var ulDiv = document.createElement('ul');
+   ulDiv.setAttribute('class', 'pagination-ul');
+   paginationDiv.appendChild(ulDiv);
+
+   //create a loop to create 'li' and 'a' for each student
+   //testing
+  //  console.log(`number of pages: ${numberOfPages}`);
+   for(let i  = 1; i <= numberOfPages; i++)
+   {
+     // console.log(`in the loop for creating 'li' and 'a' for each student`);
+
+     //create a variable to create and store 'li' in 'ulDiv' and give it class 'pagiation-li'
+     var liPagination = document.createElement('li');
+     liPagination.setAttribute('class', 'pagination-li');
+     ulDiv.appendChild(liPagination);
+
+     //create a variable to create and store 'a' in 'li' and give it class 'pagination-a'
+     var aPagination = document.createElement('a');
+     aPagination.setAttribute('class', 'pagination-a');
+     //add page number
+     aPagination.textContent = (i);
+     liPagination.appendChild(aPagination);
+
+     //when the page initially loads, it should always start on page 1, so the active page should always start on page 1
+     //testing
+     // console.log(`current page number = ${currentPageNumber}`);
+
+     //make sure that active page is 1 and set the colour to it for being the active page
+     if(currentPageNumber === 1){
+        aPagination.classList.add('active');
+        //change currentPageNumber value so only page 1 is active
+        currentPageNumber++;
+     }
+   }
+
+   //So this is to make sure that the active is cleared out of all buttons and assigned to the one that's pressed, but I was having a lot of trouble. After getting help on Slack, @Juan L, helped by giving me a little bit of a psuedo code for this part to fix my issues
+
+   //Gather all the 'a' tags and store in a variable (suggested by @Juan L)
+   const aLinks = document.querySelectorAll('a');
+   //testing
+  //  console.log(aLinks);
+
+   //create a loop(i) to add eventListeners for those a tags (suggested by @Juan L)
+   for(let i = 0; i < aLinks.length; i++){
+     //  console.log(`in the i loop`)
+      // When a tag is clicked (suggested by @Juan L)
+      aLinks[i].addEventListener('click', (e) =>{
+         //Add an inner loop (j) to loop through all the 'a' tags to remove the classes (suggested by @Juan L)
+
+         //had to take out the for loop (j) suggestion, because after trying out a bunch of different variations of the for loop (j) it didn't work, but works without it.
+         //for(let j = 0; j < aLinks.length; j++){
+            //console.log(`in the j loop`);
+            
+            //create a variable to store the the 'a'
+            const activePage = document.querySelector('.active');
+            //testing
+           //  console.log(activePage);
+            
+            //remove the 'active' by changing it to ''(suggested by @Juan L)
+            activePage.className = ('');
+            //testing
+            //console.log(`leaving the j loop`);
+         //}
+
+         //Back in the outer loop add back the class of active to whatever link was clicked and call showPage (suggseted by @Juan L)
+
+         //create a variable to get and store which button was clicked
+         const buttonClicked = e.target;
+
+         //update current page number to pass into the showPage function
+         currentPageNumber = buttonClicked.textContent;
+         //testing
+        //  console.log(`page number is: ${currentPageNumber}`);
+
+         //add 'active' class onto the button clicked
+         buttonClicked.className = 'active';
+        
+         //display the students based on the page number in currentPageNumber
+         showPage(listOfStudents, currentPageNumber);
+      });
+   }
+
 }
 
 //call for showPage function
