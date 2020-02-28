21893a84fe92f73736781c56d85b13f581aeb0f9
Winnie Kelley <alldayallnight642@icloud.com>
2020-02-27 23:59:26 -0500

    fixed error that kept happening after no match found. updated comments even more. and added # to href in a

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
  This is project 2 of Treehouse FSJS Techdegree - List Pagination and Filtering
 
  I am going for the grade of exceeding expectations
+Thank you @Juan L, @Emma W, @Lee V and @Jammie Gobeille for all the help 
 
  Treehouse provided me with these starter project files:
  - HTML files:
@@ -44,80 +45,49 @@ To exceed expectations Treehouse would like the following:
 Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
 BREAK DOWN:
-/**
- * DISPLAYING A PAGE
- * (Using console to test the function in the earlier stages)
- * 1. Create a function that:
- *    1a. function hidides all the students other than the 10 
- *        students that are being displayed.
- *    1b. function should have two parameter:
- *       - list : the list of students that's being passed in 
- *                as an argument when calling the function.
- *       - page : the page number that's being passed in as an 
- *                argument when calling the function.
- * 
- * 2. Create two variables (This is so it works with any 
- *    length list).
- *    2a. Start Index = (page parameter * items per page) - 
- *       items per page.
- *    2b. End Index = page parameter * items per page
- * 
- * 3. Create a loop that loops over the list
- *    3a. display items that are:
- *       - greater than or equal to start index
- *       AND
- *       - less than the end index.
- *    3b. hide items that doesn't fit above parameters
- */
 
- 
-/**
- * ADDING PAGINATION LINKS
- * 1. Create a function that:
- *    1a. function that creates and appends functioning
- *        pagination links.
- *    1b. accepts a single "list" parameter to represent the 
- *        actual list of students.
- *    1c. Inside the function:
- *       - Create and append DOM elements for pagination links;
- *       - use lines 119 - 137 in examples/example-meets.thml 
- *         as template.
- *       - watch out for:
- *          - how elements are nested.
- *          - necessary class names.
- *          - other elements attributes.
- *          - where additions should be append.
- * 
- * 2. Add active class to the first pagination link initially.
- * 3. Add a "click" event listneer to each A element. A loop 
- *    can be helpful here.
- *    3a. When the pagination link is clicked:
- *       - active class name should be removed from all 
- *         pagination links (use a loop).
- *       - active calss name should be added to the link that 
- *         was just clicked. (use target property of the 
- *         event object).
- *       - showPage function should be called passing in 
- *         arguments
- * 
- * RESULTS:
- * - container div element with class name "pagination"
- *    - append to div element with class name of page.
- * - nested UL element containing one LI element for every 10 
- *    students in the list.
- * 
- * TIP:
- * Divide the list.length by the max number of items per page 
- * to figure out how many pages are needed, and can use a loop 
- * that iterates that many times to create the correct number 
- * of LI elements.
- * 
- * - Each LI element should contain an A element with an href 
- * attribute of # and text set to the page number each link 
- * will show. First link is 1. Second link is 2. And so on.
- * 
- * - Loop index can be helpful in setting the text of the 
- * pagination links.
- * 
- */
+DISPLAYING A PAGE
+- (Using console to test the function in the earlier stages)
+1. Create a function that:
+    1a. function hidides all the students other than the 10 students that are being displayed.
+    1b. function should have two parameter:
+        - list : the list of students that's being passed in as an argument when calling the function.
+        - page : the page number that's being passed in as an argument when calling the function.
+2. Create two variables (This is so it works with any length list).
+    2a. Start Index = (page parameter * items per page) - items per page.
+    2b. End Index = page parameter * items per page
+3. Create a loop that loops over the list
+    3a. display items that are:
+        - greater than or equal to start index
+        AND
+        - less than the end index.
+    3b. hide items that doesn't fit above parameters
 
+ADDING PAGINATION LINKS
+1. Create a function that:
+    1a. function that creates and appends functioning pagination links.
+    1b. accepts a single "list" parameter to represent the actual list of students.
+    1c. Inside the function:
+        - Create and append DOM elements for pagination links;
+        - use lines 119 - 137 in examples/example-meets.html as template.
+        - watch out for:
+            - how elements are nested.
+            - necessary class names.
+            - other elements attributes.
+            - where additions should be append.
+2. Add active class to the first pagination link initially.
+3. Add a "click" event listneer to each A element. A loop can be helpful here.
+    3a. When the pagination link is clicked:
+        - active class name should be removed from all pagination links (use a loop).
+        - active calss name should be added to the link that was just clicked. (use target property of the event object).
+        - showPage function should be called passing in arguments
+
+RESULTS:
+- container div element with class name "pagination"
+- append to div element with class name of page.
+- nested UL element containing one LI element for every 10 students in the list.
+
+TIP:
+- Divide the list.length by the max number of items per page to figure out how many pages are needed, and can use a loop that iterates that many times to create the correct number of LI elements.
+- Each LI element should contain an A element with an href attribute of # and text set to the page number each link will show. First link is 1. Second link is 2. And so on.
+- Loop index can be helpful in setting the text of the pagination links.
--- js/script.js
+++ js/script.js
@@ -18,65 +18,44 @@ Psuedo code written based on Treehouse Study guide and Instructions
  * students.
  */
 //Global variables:
-
-// variable to get and store the list of students:
 const listOfStudents = document.querySelectorAll('.student-item');
-
-//variable to store the max number of students that can be shown per page (10)
 const numberOfStudentsPerPage = 10;
-
-//variable for figuring out what page the user is on
 let currentPageNumber = 1;
 
-//create a variable to get and store the '.page' div
 var pageDiv = document.querySelector('.page');
-
-//create a div and store it for no match found message
 const noMatchesFoundDiv = document.createElement('div');
 
-//create a search bar using lines 16 - 19 from examples/example-exceeds.html as a template
-//create a variable to get and store 'page-header'
 var pageHeaderDiv = document.querySelector('.page-header');
 
-//create and store search bar div as a 'student-search' class
 var studentSearchDiv = document.createElement('div');
 studentSearchDiv.setAttribute('class', 'student-search');
 pageHeaderDiv.appendChild(studentSearchDiv);
 
-//create and store input element with placeholder: 'Search for students'
 var userSearchInput = document.createElement('input');
 userSearchInput.setAttribute('placeholder', 'Search for students');
 studentSearchDiv.appendChild(userSearchInput);
 
-//create and store a search button
 const searchButton = document.createElement('button');
 searchButton.textContent = 'Search';
 studentSearchDiv.appendChild(searchButton);
 
-//add searchButton eventListener for click:
+//eventListeners for click and keyup
 searchButton.addEventListener('click', (e)=>{
    search(userSearchInput.value);
 });
 
-//add input eventListener for up key:
 userSearchInput.addEventListener('keyup', (e) => {
    search(e.target.value);
 });
 
 //function for search button
 const search = (userInputValue) =>{
-   //variable to store information for searching students
    let searchList = [];
-   //get all the names in the list
    var allNames = document.querySelectorAll('.student-details h3');
-   //create a loop to compare user input to each student
    for(let i = 0; i < listOfStudents.length; i++){
       listOfStudents[i].style.display = 'none';
-      //create a variable to store a name one name at a time
       var name = allNames[i];
-      //compare user input to list of students:
       if(name.textContent.toLowerCase().includes(userSearchInput.value.toLowerCase())){
-         //put the person onto searchList
          searchList.push(listOfStudents[i]);
       }
    }
@@ -84,8 +63,12 @@ const search = (userInputValue) =>{
    appendPageLinks(searchList);
 }
 
-//DISPLAYING A PAGE:
-//Create a function showPage, that has two parameters (list, page) 
+/**
+ * displaying a page
+ * ceate function that hides all students except for the 10 that's being displayed
+ * function should have two parameters, list and page
+ * also displays error message for search if no match found
+ */
 const showPage = (anyList, page) => {
    //create 2 variables startIndex and endIndex so that this will work for any length list of students (variables and list provided by Treehouse)
    const startIndex = (page * numberOfStudentsPerPage) - numberOfStudentsPerPage;
@@ -93,100 +76,77 @@ const showPage = (anyList, page) => {
    if(anyList.length === 0){
       noMatchesFoundDiv.style.display = '';
       noMatchesFoundDiv.textContent = "Sorry. There are no matches found. Please try again."
-      //add this to pageDiv
       pageDiv.appendChild(noMatchesFoundDiv);
    } else{
-      //hide the error message
       noMatchesFoundDiv.style.display = 'none';
    }
 
-   //create a loop that will loop over the list
    for (let i = 0; i < anyList.length; i++){
-      //create an if statement to check to see where we are in the loop so we can display 10 students and hide the rest
       if ( i >= startIndex && i < endIndex){
-         //display these students
          anyList[i].style.display = '';
       } else{
-         //hide the students beyound the 10 that's being displayed
          anyList[i].style.display = 'none';
       }
    }
 }
 
+/**
+ * adding pagination links
+ * create function that appends page links at the bottom of the page
+ * should accept any length list
+ * uses line 119 - 137 in examples/example-meets.html as template
+ * should have an active class for the initial page that it's on then active on the page that's clicked on
+ */
 const appendPageLinks = (listOfStudents) => {
-  //hide pagination
   var clearPagination = document.querySelector('.pagination');
   if (clearPagination){
      clearPagination.remove();
   }
 
-   //create a variable to store the total number of pages needed
    const numberOfPages = Math.ceil(listOfStudents.length / numberOfStudentsPerPage);
-   //Using lines 119 - 137 in examples/example-meets.html as a template:
-   //create a variable to get and store the '.page' div
+
    var pageDiv = document.querySelector('.page');
-   //create a variable and create a div in it and call it "pagination"
    var paginationDiv = document.createElement('div');
    paginationDiv.setAttribute('class', 'pagination');
    pageDiv.appendChild(paginationDiv);
    
-   //create variable to create a 'ul' in paginationDiv and give it a class of 'pagination-ul'
    var ulDiv = document.createElement('ul');
    ulDiv.setAttribute('class', 'pagination-ul');
    paginationDiv.appendChild(ulDiv);
 
-   //create a loop to create 'li' and 'a' for each student
    for(let i  = 1; i <= numberOfPages; i++)
    {
-     //create a variable to create and store 'li' in 'ulDiv' and give it class 'pagiation-li'
      var liPagination = document.createElement('li');
      liPagination.setAttribute('class', 'pagination-li');
      ulDiv.appendChild(liPagination);
 
-     //create a variable to create and store 'a' in 'li' and give it class 'pagination-a'
      var aPagination = document.createElement('a');
      aPagination.setAttribute('class', 'pagination-a');
-     //add page number
+     aPagination.href = '#';
      aPagination.textContent = (i);
      liPagination.appendChild(aPagination);
 
-     //make sure that active page is 1 and set the colour to it for being the active page
      if(currentPageNumber === 1){
         aPagination.classList.add('active');
-        //change currentPageNumber value so only page 1 is active
         currentPageNumber++;
      }
    }
-   //So this is to make sure that the active is cleared out of all buttons and assigned to the one that's pressed, but I was having a lot of trouble. After getting help on Slack, @Juan L, helped by giving me a little bit of a psuedo code for this part to fix my issues
 
-   //Gather all the 'a' tags and store in a variable (suggested by @Juan L)
    const aLinks = document.querySelectorAll('a');
-
-   //create a loop(i) to add eventListeners for those a tags (suggested by @Juan L)
+   const firstALink = aLinks[0];
+   if(firstALink){
+      firstALink.className = ('active');
+   }
    for(let i = 0; i < aLinks.length; i++){
-      // When a tag is clicked (suggested by @Juan L)
       aLinks[i].addEventListener('click', (e) =>{
-         //create a variable to store the the 'a'
          const activePage = document.querySelector('.active');
-            
-         //remove the 'active' by changing it to ''(suggested by @Juan L)
          activePage.className = ('');
-         
-         //create a variable to get and store which button was clicked
          const buttonClicked = e.target;
-
-         //update current page number to pass into the showPage function
          currentPageNumber = buttonClicked.textContent;
-         
-         //add 'active' class onto the button clicked
          buttonClicked.className = 'active';
-        
-         //display the students based on the page number in currentPageNumber
          showPage(listOfStudents, currentPageNumber);
       });
    }
 }
-//call for showPage function
 showPage(listOfStudents, 1);
-//call appendPageLinks function
 appendPageLinks(listOfStudents); 
